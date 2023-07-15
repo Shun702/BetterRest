632aed81a30e48651e7a3246bac43a71a4bdb7f1
Shun702 <138392252+Shun702@users.noreply.github.com>
2023-07-15 19:19:59 +0100

    initial commit
    raw code added

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Shun Le Yi Mon on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
